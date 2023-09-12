e97b67cdf68490d5a9136678971b75b6784b288c
critipan <1524506135@qq.com>
2023-09-12 21:51:17 +0800

    添加物品

---
+++ prototypes/item/arc-reactor.lua
@@ -0,0 +1,12 @@
+data:extend({
+    {
+        type = "item",
+        name = "arc-reactor",
+        icon = "__FactorioArcReactor__/graphics/icons/arc-reactor-icon.png",
+        icon_size = 32,
+        placed_as_equipment_result = "arc-reactor",
+        subgroup = "equipment",
+        order = "a[energy-source]-c[arc-reactor]",
+        stack_size = 1
+    }
+})
