a177cf461212b2a88070df78fb2cb96b48a5cc58
critipan <1524506135@qq.com>
2023-09-12 21:51:26 +0800

    添加合成表

---
+++ prototypes/recipe/arc-reactor.lua
@@ -0,0 +1,13 @@
+data:extend({
+    {
+        type = "recipe",
+        name = "arc-reactor",
+        enabled = false,
+        energy_required = 20,
+        ingredients = {
+            {"fusion-reactor-equipment", 1},
+            {"processing-unit", 100},
+        },
+        result = "arc-reactor"
+    }
+})
